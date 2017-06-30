33418c2cddd78a425682a1282d3eb4480735f365
Marco Montanari <sirmmo@gmail.com>
2017-06-30 15:00:53 +0200

    generification of mn-minio
    made all elements generic

--- minio-conf.php
+++ minio-conf.php
@@ -3,24 +3,26 @@
 Plugin Name: Modal Nodes Minio Configurator
 */
 
-//http://cdn.modal-nodes.com/
-//Access Key: JNADJ91V2BJ0PW07S8VG
-//Secret Key: EwHhU+LjVq11OFp7vnBsEkJ7I74RkHMg6QKp8G0m
+$minio_url = getenv('MINIO_URL'); //http://cdn.example.com/
+$minio_key = getenv('MINIO_KEY'); //
+$minio_secret = getenv('MINIO_SECRET');
+$minio_bucket = getenv('MINIO_BUCKET'); // testbucket
+$minio_bucket_url = getenv('MINIO_BUCKET_URL'); //http://cdn.example.com/testbucket
 
 function s3_uploads_s3_client_params_runner( $params ) {
-  $params['endpoint'] = 'http://cdn.modal-nodes.com/';
+  $params['endpoint'] = $minio_url;
   $params['credentials'] = [
-                'key'    => 'JNADJ91V2BJ0PW07S8VG',
-                'secret' => 'EwHhU+LjVq11OFp7vnBsEkJ7I74RkHMg6QKp8G0m',
+                'key'    => $minio_key,
+                'secret' => $minio_secret,
             ];
   return $params;
 }
 add_filter( 's3_uploads_s3_client_params', 's3_uploads_s3_client_params_runner', 20, 1 );
 
-define( 'S3_UPLOADS_BUCKET', 'filocorto' );
-define( 'S3_UPLOADS_KEY', 'JNADJ91V2BJ0PW07S8VG' );
-define( 'S3_UPLOADS_SECRET', 'EwHhU+LjVq11OFp7vnBsEkJ7I74RkHMg6QKp8G0m' );
+define( 'S3_UPLOADS_BUCKET', $minio_bucket );
+define( 'S3_UPLOADS_KEY', $minio_key );
+define( 'S3_UPLOADS_SECRET', $minio_secret );
 define( 'S3_UPLOADS_REGION', 'us-east-1' );
-define( 'S3_UPLOADS_BUCKET_URL', 'http://cdn.modal-nodes.com/filocorto' );
+define( 'S3_UPLOADS_BUCKET_URL', $minio_bucket_url);
 
 ?>
