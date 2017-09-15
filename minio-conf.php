9c11f4b039ec530d91f1e774f2e43b503aa4f2b5
Marco Montanari <sirmmo@gmail.com>
2017-09-15 20:09:28 +0200

    Fix for latest version
    Updated based on https://github.com/humanmade/S3-Uploads/issues/184

--- minio-conf.php
+++ minio-conf.php
@@ -11,20 +11,31 @@ $minio_secret = getenv('MINIO_SECRET');
 $minio_bucket = getenv('MINIO_BUCKET'); // testbucket
 $minio_bucket_url = getenv('MINIO_BUCKET_URL'); //http://cdn.example.com/testbucket
 
+if(!$minio_bucket_url){
+  $minio_bucket_url = $minio_url."/".$minio_bucket;
+}
+
+define('S3_UPLOADS_BUCKET', $minio_bucket);
+define('S3_UPLOADS_KEY', $minio_key);
+define('S3_UPLOADS_SECRET', $minio_secret);
+define('S3_UPLOADS_REGION', 'us-east-1');
+define('S3_UPLOADS_BUCKET_URL', $minio_bucket_url);
+
 function s3_uploads_s3_client_params_runner( $params ) {
+  $minio_url = getenv('MINIO_URL'); //http://cdn.example.com/
+  $minio_key = getenv('MINIO_KEY'); //
+  $minio_secret = getenv('MINIO_SECRET');
+  $minio_bucket = getenv('MINIO_BUCKET'); // testbucket
+  $minio_bucket_url = getenv('MINIO_BUCKET_URL'); //http://cdn.example.com/testbucket
   $params['endpoint'] = $minio_url;
+  $params['use_path_style_endpoint'] = true;
   $params['credentials'] = [
                 'key'    => $minio_key,
                 'secret' => $minio_secret,
             ];
   return $params;
 }
-add_filter( 's3_uploads_s3_client_params', 's3_uploads_s3_client_params_runner', 20, 1 );
 
-define( 'S3_UPLOADS_BUCKET', $minio_bucket );
-define( 'key', $minio_key );
-define( 'secret', $minio_secret );
-define( 'S3_UPLOADS_REGION', 'us-east-1' );
-define( 'S3_UPLOADS_BUCKET_URL', $minio_bucket_url);
+add_filter( 's3_uploads_s3_client_params', 's3_uploads_s3_client_params_runner', 20, 1 );
 
 ?>
